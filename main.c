3e2f3616f024ff3fdbabb9c401079d0807a67145
emnaaaaaaaaa <140275314+emnaaaaaaaaa@users.noreply.github.com>
2024-05-01 10:27:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ int main(void)
     GPIO_Init(GPIO_D,MODE_OUTPUT,OPEN_DRAIN,2);
     GPIO_WriteBit(GPIO_D,2,1);
    
-    GPIO_Init(GPIO_D,MODE_INPUT,OPEN_DRAIN,2); 
-    unsigned  char read_input_pin = GPIO_ReadInputDataBit(GPIO_D,2);
+    //GPIO_Init(GPIO_D,MODE_INPUT,OPEN_DRAIN,2); 
+    //unsigned  char read_input_pin = GPIO_ReadInputDataBit(GPIO_D,2);
     
-    unsigned  short int read_input = GPIO_ReadInputData(GPIO_D);
+    //unsigned  short int read_input = GPIO_ReadInputData(GPIO_D);
     
     
    
-    GPIO_Write(GPIO_D,12);
+    //GPIO_Write(GPIO_D,12);
     
     while (1){}
  }
